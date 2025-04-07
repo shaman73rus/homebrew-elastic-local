c5a0e19286ff828981d3e8b7c7fa9ac4cd3af40c
Nikita Khvatkov <vtb4112767@region.vtb.ru>
2025-04-07 18:58:57 +0400

    Fix service block: use 'run' instead of 'command'

--- Formula/elasticsearch-local.rb
+++ Formula/elasticsearch-local.rb
@@ -6,15 +6,16 @@ class ElasticsearchLocal < Formula
   sha256 "06ee63e50b77e7d88b5292b7fb173564e069b453cd6a7e948570f290714b62ac"
 
   def install
-    prefix.install Dir["*"]
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/elasticsearch"
   end
 
   service do
-    command [opt_bin/"elasticsearch"]
+    run [opt_bin/"elasticsearch"]
     environment_variables(
       "JAVA_HOME" => "/opt/homebrew/opt/openjdk@17"
     )
-    working_dir opt_prefix
+    working_dir var
     log_path var/"log/elasticsearch-local.log"
     error_log_path var/"log/elasticsearch-local-error.log"
   end
